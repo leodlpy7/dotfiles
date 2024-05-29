d0674ddc525bdbccbe20bbadbaa620c6caeac0bc
Leonard Delpy <leonard.delpy@gmail.com>
2024-05-29 20:37:19 +0200

    modified pkgs url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "laptop nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     flake-utils.url = "github:numtide/flake-utils";
   };
