2a9cd7ed9ba202e2718bc06b11b86baed06e2895
Leonard Delpy <leonard.delpy@gmail.com>
2025-04-17 21:27:55 +0200

    added yubikey

---
+++ machines/share/yubikey.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+
+  security.pam.yubico = {
+    enable = true;
+    debug = true;
+    mode = "challenge-response";
+    id = [ "33038951" ];
+  };
+
+  # lock session if yubikey is unplugged
+  services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
+}
